0d79f672b8ca9c904000f5b63a9036490a7fb03d
Kipkoech <nickey968@gmail.com>
2023-05-15 10:46:21 +0300

    first commit

---
+++ src/components/EventContainer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const EventsContainer = ({ name, desc, date, organizer, ticketType }) => {
+    return (
+        <div className=''>
+            <div className="">{name}</div>
+        </div>
+    )
+}
+
+export default EventsContainer
---
+++ src/components/Events.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import EventsContainer from './EventContainer'
+
+const Events = () => {
+    return (
+        <div>
+            <div className='events container-xl mt-5'>
+                <div className='event-box'>hello</div>
+                <div className='event-box'>hello</div>
+                <div className='event-box'>hello</div>
+            </div>
+        </div>
+    )
+}
+
+export default Events
